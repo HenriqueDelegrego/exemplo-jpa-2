b9d7ed78917f86d0c716a0c54a84c0f592787b30
Henrique <62546083+HenriqueDelegrego@users.noreply.github.com>
2025-10-04 21:17:13 -0300

    Initial commit

---
+++ src/main/java/com/delegrego/exemplo_jpa_2/ExemploJpa2Application.java
@@ -0,0 +1,13 @@
+package com.delegrego.exemplo_jpa_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemploJpa2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExemploJpa2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/delegrego/exemplo_jpa_2/ExemploJpa2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delegrego.exemplo_jpa_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemploJpa2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
