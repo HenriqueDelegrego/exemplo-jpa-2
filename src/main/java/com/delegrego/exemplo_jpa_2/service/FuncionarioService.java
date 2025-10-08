6141afbb527b0934adb8094124f896d3ae3f1160
Henrique Delegrego <62546083+HenriqueDelegrego@users.noreply.github.com>
2025-10-08 16:11:35 -0300

    style: adiciona linha em branco

--- src/main/java/com/delegrego/exemplo_jpa_2/service/FuncionarioService.java
+++ src/main/java/com/delegrego/exemplo_jpa_2/service/FuncionarioService.java
@@ -21,6 +21,7 @@ import jakarta.validation.Valid;
 
 // Indica que esta classe é um serviço do Spring (camada de lógica de negócio)
 @Service
+
 // Habilita a validação de métodos nesta classe
 @Validated
 public class FuncionarioService {
@@ -101,7 +102,8 @@ public class FuncionarioService {
 				.orElseThrow(() -> new RuntimeException("Funcionário não existe"));
 
 		// TODO: Refatorar?
-		if (funcionarioRepo.existsByEmailAndIdFuncionarioNot(funcionarioDto.getEmail(), funcionarioDto.getIdFuncionario())) {
+		if (funcionarioRepo.existsByEmailAndIdFuncionarioNot(funcionarioDto.getEmail(),
+				funcionarioDto.getIdFuncionario())) {
 			throw new RuntimeException("Usuário com esse email já existe");
 		}
 
